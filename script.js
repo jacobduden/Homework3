ce374bcf0d26f677f38e2ac204f7f602bec99fb8
Jacob Duden <Jlavaball@gmail.com>
2021-01-04 19:57:15 -0600

    fixed variables that were reading false true

--- script.js
+++ script.js
@@ -10,56 +10,60 @@ function writePassword() {
  
  }
  function generatePassword() {
-
  
- var passwordLength = Number(prompt("How many characters would you like in your password?"));
- console.log(passwordLength)
+      var passwordLength = Number(prompt("How many characters would you like in your password?"));
+      console.log(passwordLength);
 
+      if (passwordLength < 8) { 
+        alert("Your password must have at least 8 characters.");
+      }
 
+      if (passwordLength > 128) {
+        alert("Your password must have less that 129 characters.");
+      }
 
- if (passwordLength < 8)  { 
-   alert("Your password must have at least 8 characters.");
- }
+      var charSet = "";
+      var passwordUpperCase = confirm("Would you like uppercase letters?");
+      console.log(passwordUpperCase);
 
- if (passwordLength > 128) {
-   alert("Your password must have less that 129 characters.");
- }
- 
- var charSet ="";
- var passwordUpperCase = confirm("Would you like uppercase letters?");
- console.log(passwordUpperCase)
+      if (passwordUpperCase == true) {
+        charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        console.log(charSet);
+      }
 
- if(passwordUpperCase = true) {
-   charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- }
+      var passwordLowerCase = confirm("Would you like lowercase letters?");
+      console.log(passwordLowerCase);
 
- var passwordLowerCase = confirm("Would you like lowercase letters?");
- console.log(passwordLowerCase)
+      if (passwordLowerCase == true) {
+        charSet += "abcdefghijklmnopqrstuvwxyz";
+        console.log(charSet);
+      }
 
-if(passwordLowerCase = true) {
-  charset = "abcdefghijklmnopqrstuvwxyz";
-}
+      var passwordNumbers = confirm("Would you like numbers?");
+      console.log(passwordNumbers);
 
- var passwordNumbers = confirm("Would you like numbers?");
- console.log(passwordNumbers)
+      if (passwordNumbers == true) {
+        charSet += "1234567890";
+        console.log(charSet);
+      }
 
- if(passwordNumbers = true) {
-   charSet = "1234567890";
- }
+      var passwordSpecial = confirm("Would you like special characters?");
+      console.log(passwordSpecial);
 
- var passwordSpecial = confirm("Would you like special characters?");
- console.log(passwordSpecial)
+      if(passwordSpecial == true) {
+        charSet += "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+        console.log(charSet);
+      }
 
- if(passwordSpecial = true) {
-   charSet = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
- }
+      var retVal = " ";
+      console.log(retVal);
 
- var retVal = "";
-  for (var i = 0; i < passwordLength; i++) {
-    //picks a character within charSet at index of random number
-    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
-  }
-  return retVal;
+        for (var i = 0; i < passwordLength; i++) {
+          //picks a character within charSet at index of random number
+          retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+        }
+        return retVal; 
+        
  }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
